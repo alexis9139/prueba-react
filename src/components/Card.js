5e27899ecc094374f675628039cea20ea9cd4258
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-06 20:41:59 -0300

    incorporando estilos a los componentes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import excerciseImg from './Images/imagen.jpg' //importamos la imagen
+import './styles/Card.css'//importamos los estilos
+import 'bootstrap/dist/css/bootstrap.css'
 class Card extends React.Component {
 
     render() {//renderiza lo que definimos dentro de la funcion
         return (
-            <div>
-                <div>
-                    <div>
-                        <img src={excerciseImg} width="80px" />
-                    </div>
-                    <div>
-                        <h1>Titulo imagen</h1>
-                        <p>Lorem imsupts</p>
+            <div className="card mx-auto Fitness-Card">
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={excerciseImg} width="80px" className="float-right" />
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>Titulo imagen</h1>
+                            <p>Lorem imsupts</p>
+                        </div>
                     </div>
                 </div>
             </div>
+
         )
     }
 }
